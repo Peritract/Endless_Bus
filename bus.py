8390521762cf3edc077365ff49d252b5815461f8
Peritract <peritract@hotmail.co.uk>
2016-02-17 20:54:42 +0000

    Update bus.py

--- bus.py
+++ bus.py
@@ -97,6 +97,7 @@ def tweet(text):
 
 #Actually does stuff.
 if __name__ == "__main__":
-    message = construct_tweet()
-    tweet(message)
-    sleep(1800)
+    while True:
+        message = construct_tweet()
+        tweet(message)
+        sleep(1800)
